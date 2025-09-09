62fea5c9f708183d92cad7a64f16df199126dd35
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-09 15:47:55 +0700

    Add LSP server for webdev

--- init.lua
+++ init.lua
@@ -483,8 +483,15 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        --
+        -- Web development
+        -- tailwindcss-language-server tailwindcss (keywords: css)
+        eslint = {},
+        html = {},
+        jsonls = {},
+        emmet_language_server = {},
+        cssls = {},
+        ts_ls = {},
 
         lua_ls = {
           -- cmd = { ... },
